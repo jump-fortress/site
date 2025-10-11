8d1b371799bec20c7744559be5e5e982ff67441b
bailey <spiritov_v@pm.me>
2025-10-11 01:32:07 -0700

    add basic website

---
+++ web/src/lib/components/Nav.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<nav class="fixed z-50 flex h-16 w-full select-none justify-center">
+	<div class="w-5xl flex items-center justify-between">
+		<!-- left nav -->
+		<div class="flex gap-x-8">
+			<a href="/ladder"><span>ladder</span></a>
+			<a href="/formats"><span>formats</span></a>
+			<a href="/help"><span>help</span></a>
+		</div>
+		<!-- right nav -->
+		<div class="flex flex-row-reverse gap-x-8">
+			<img class="cursor-pointer" src="src/lib/assets/static/sits_small.png" alt="" />
+		</div>
+	</div>
+</nav>
