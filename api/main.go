c2a5c756aaf72104cf5616b90f4b7b0f4ab3c511
bailey <spiritov_v@pm.me>
2025-10-11 01:34:47 -0700

    add basic api

--- api/main.go
+++ api/main.go
@@ -5,7 +5,10 @@ import (
 	"database/sql"
 	_ "embed"
 	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 	database "github.com/spiritov/jump/api/db/queries"
 )
@@ -19,16 +22,47 @@ func main() {
 
 	queries := database.New(db)
 	ctx := context.Background()
+
 	player, err := queries.InsertPlayer(ctx, database.InsertPlayerParams{
-		SteamID:         "test",
-		DisplayName:     "spiritov",
-		SoldierDivision: sql.NullString{String: "div", Valid: true},
-		DemoDivision:    sql.NullString{},
+		SteamID:     "1",
+		SteamPfpID:  "1",
+		DisplayName: "spiritov",
 	})
 	if err != nil {
-		log.Printf("[error] failed to insert player: %v", err)
+		//log.Fatal(err)
 	} else {
-		log.Printf("got player: %+v", player)
+		log.Println(player)
 	}
 
+	r := gin.Default()
+
+	r.GET("/players/:id", func(c *gin.Context) {
+		idParam := c.Param("id")
+		_, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			return
+		}
+
+		// select player from db
+		player, err := queries.SelectPlayer(ctx, 1)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "player not found"})
+		}
+
+		c.JSON(http.StatusOK, player)
+	})
+
+	r.POST("/players", func(c *gin.Context) {
+		var player database.InsertPlayerParams
+
+		if err := c.BindJSON(&player); err != nil {
+			return
+		}
+
+		// add player to db
+		queries.InsertPlayer(ctx, player)
+		c.JSON(http.StatusCreated, player)
+	})
+
+	r.Run("localhost:8080")
 }
