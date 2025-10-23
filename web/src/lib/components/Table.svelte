f912d694e46ffb7a38f9b902b98bada1183a7271
bailey <spiritov_v@pm.me>
2025-10-23 03:52:55 -0700

    add data table component

---
+++ web/src/lib/components/Table.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		data: object[];
+		header: Snippet;
+		row: Snippet<object[]>;
+	}
+
+	let { data, header, row }: { data: object[]; header: Snippet; row: Snippet<[any]> } = $props();
+</script>
+
+<table class="bg-jfgray-900 mt-2 table-fixed rounded-t-lg">
+	<thead>
+		<tr class="border-ctp-lavender/50 h-8 border-b-2 text-2xl/4">
+			{@render header()}
+		</tr>
+	</thead>
+
+	<tbody>
+		<tr class="even:bg-jfgray-800/75 odd:bg-jfgray-900 h-12 text-center">
+			{#each data as d}
+				{@render row(d)}
+			{/each}
+		</tr>
+	</tbody>
+</table>
