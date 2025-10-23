c5373c2c3b484a1375bccba6fa6023cba4004311
bailey <spiritov_v@pm.me>
2025-10-23 03:51:53 -0700

    add data section component

---
+++ web/src/lib/components/DataSection.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children, title }: { children: Snippet; title: string } = $props();
+</script>
+
+<div class="flex flex-col gap-1">
+	<span class="text-ctp-lavender">{title}</span>
+	{@render children()}
+</div>
