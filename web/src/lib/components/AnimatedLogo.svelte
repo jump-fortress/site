da7d50ea0c00b334ecbec63cec9854d368966734
bailey <spiritov_v@pm.me>
2025-10-23 03:51:42 -0700

    animated logo components

---
+++ web/src/lib/components/AnimatedLogo.svelte
@@ -0,0 +1,12 @@
+<script>
+	import overlay from '$lib/assets/static/logo/anim/overlay.png';
+	import mask from '$lib/assets/static/logo/anim/mask.png';
+	import bg from '$lib/assets/static/logo/anim/bg.png';
+</script>
+
+<div class="size-128 animate-[spin_120s_linear_infinite] select-none">
+	<img class="absolute z-10" src={overlay} alt="" />
+	<div style={`mask-image: url(${mask})`} class="mask-alpha mask-center mask-no-repeat">
+		<img class="animate-[reverse-spin_120s_linear_infinite]" src={bg} alt="" />
+	</div>
+</div>
---
+++ web/src/lib/components/AnimatedLogoSm.svelte
@@ -0,0 +1,16 @@
+<script>
+	import overlay from '$lib/assets/static/logo/anim/small/overlay.png';
+	import mask from '$lib/assets/static/logo/anim/small/mask.png';
+	import bg from '$lib/assets/static/logo/anim/small/bg.png';
+</script>
+
+<div class="size-16 animate-[spin_120s_linear_infinite]">
+	<img class="absolute z-10" src={overlay} alt="" draggable="false" />
+	<div
+		style={`mask-image: url(${mask})`}
+		class="
+   mask-alpha mask-center mask-no-repeat"
+	>
+		<img class="animate-[reverse-spin_120s_linear_infinite]" src={bg} alt="" draggable="false" />
+	</div>
+</div>
