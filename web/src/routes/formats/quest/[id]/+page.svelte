e020a822b77b5bb4216f100afbc8e7686ec3c1fa
bailey <spiritov_v@pm.me>
2025-10-28 18:34:05 -0700

    update quest dummy data

--- web/src/routes/formats/quest/[id]/+page.svelte
+++ web/src/routes/formats/quest/[id]/+page.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
 	import DataSection from '$lib/components/DataSection.svelte';
+	import Link from '$lib/components/Link.svelte';
 	import Table from '$lib/components/Table.svelte';
-
+	import TableMap from '$lib/components/TableMap.svelte';
 	import rocket from '$lib/assets/static/classes/rocket.png';
 	import sticky from '$lib/assets/static/classes/sticky.png';
 	import FullDivider from '$lib/components/FullDivider.svelte';
-	import TableMap from '$lib/components/TableMap.svelte';
-	import Link from '$lib/components/Link.svelte';
-	import DivisionTag from '$lib/components/DivisionTag.svelte';
 </script>
 
-<DataSection title="Active Quests">
+<DataSection title={'Details'}>
+	<span class="text-2xl/4">quest #1</span>
 	<Table data={[{}]}>
 		{#snippet header()}
-			<th class="w-12"></th>
+			<th class="w-14"></th>
 			<th class="w-1/10"></th>
 			<th class="w-1/10">amount</th>
 			<th class="w-1/12">claimed</th>
@@ -21,46 +20,36 @@
 			<th class="w-1/10">target time</th>
 			<th class="w-1/6">placed by</th>
 			<th class="w-1/10">end date</th>
-			<th class="w-14">results</th>
 		{/snippet}
 		{#snippet row()}
 			<td><img src={rocket} class="m-auto size-8" alt="" /></td>
-			<td><div class="flex justify-center"><DivisionTag div="diamond" /></div></td>
+			<td class="text-ctp-teal text-left">Platinum</td>
 			<td class="text-shadow-xs text-shadow-ctp-lavender">5 keys</td>
 			<td>0/20</td>
 			<td class="relative"><TableMap map="jump_beef" /></td>
 			<td>completion</td>
 			<td>jrjrjr</td>
 			<td>10/23/25</td>
-			<td><Link href="ababa" title="ababa" /></td>
 		{/snippet}
 	</Table>
 </DataSection>
 
 <FullDivider />
 
-<DataSection title="Past Quests">
+<DataSection title={'Results'}>
 	<Table data={[{}]}>
 		{#snippet header()}
-			<th class="w-12"></th>
-			<th class="w-1/10"></th>
-			<th class="w-1/10">amount</th>
-			<th class="w-1/12">claimed</th>
-			<th>map</th>
-			<th class="w-1/10">target time</th>
-			<th class="w-1/6">placed by</th>
-			<th class="w-1/10">end date</th>
-			<th class="w-14">results</th>
+			<th class="w-14">#</th>
+			<th class=""></th>
+			<th class="w-1/8">run time</th>
+			<th class="w-1/10">run date</th>
+			<th class="w-14">link</th>
 		{/snippet}
 		{#snippet row()}
-			<td><img src={rocket} class="m-auto size-8" alt="" /></td>
-			<td><div class="flex justify-center"><DivisionTag div="diamond" /></div></td>
-			<td class="text-shadow-xs text-shadow-ctp-lavender">5 keys</td>
-			<td>0/20</td>
-			<td class="relative"><TableMap map="jump_beef" /></td>
-			<td>completion</td>
-			<td>jrjrjr</td>
-			<td>10/23/25</td>
+			<td>1</td>
+			<td class="text-ctp-sky text-left">mur</td>
+			<td>00:54.23</td>
+			<td>12/23/25</td>
 			<td><Link href="/formats/quest/1" title="results" /></td>
 		{/snippet}
 	</Table>
