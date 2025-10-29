a261337709a5f523f4fcc92972b544a2c1b3aae3
bailey <spiritov_v@pm.me>
2025-10-28 18:35:47 -0700

    add Link component

---
+++ web/src/lib/components/Link.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { href, title }: { href: string; title: string } = $props();
+</script>
+
+<a {href} {title} class="opacity-50 transition-opacity hover:opacity-100">
+	<span class="icon-[ri--link-m] size-6"></span></a
+>
