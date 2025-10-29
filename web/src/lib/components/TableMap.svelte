651647b58d823b42d0f56b23d89826825abd2478
bailey <spiritov_v@pm.me>
2025-10-28 18:35:54 -0700

    add TableMap component

---
+++ web/src/lib/components/TableMap.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { map }: { map: string } = $props();
+</script>
+
+<span class="relative z-10 overflow-ellipsis text-nowrap">{map}</span>
+<div
+	style={`background-image: url("https://tempusplaza.xyz/map-backgrounds/${map}.jpg")`}
+	class="mask-x-from-80% absolute top-0 size-full bg-cover bg-right bg-no-repeat opacity-50"
+></div>
