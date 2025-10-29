a6036900409118015e86a0b3162255c480580a25
bailey <spiritov_v@pm.me>
2025-10-28 18:35:41 -0700

    add competition preview component

---
+++ web/src/lib/components/DivisionTag.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		div: string;
+		style?: string;
+	}
+
+	let { div, style = '' }: Props = $props();
+</script>
+
+<span
+	class="rounded-full px-2 pb-0.5 pt-1 text-2xl/4 border-division-{div} border-x-3 text-division-{div} bg-jfgray-900/75 {style}"
+	>{div}</span
+>
