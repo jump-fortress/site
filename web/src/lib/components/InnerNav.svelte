da208517ca9a56ee7f827859213c5a4f8a65cb0e
bailey <spiritov_v@pm.me>
2025-10-28 18:32:12 -0700

    generalize FormatsNav

---
+++ web/src/lib/components/InnerNav.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let { route, parentRoute, pages }: { route: string; parentRoute: string; pages: string[] } =
+		$props();
+</script>
+
+<div class="bg-jfgray-800/90 sticky top-16 z-20 backdrop-blur-sm">
+	<div class="hover:text-ctp-lavender-50/75 relative flex h-12 w-fit transition-colors">
+		<hr
+			class="border-1 border-jfgray-700 absolute bottom-3 left-0 right-0 m-auto w-11/12 rounded-full"
+		/>
+		{#each pages as name}
+			{@render FormatPage(name)}
+		{/each}
+	</div>
+</div>
+
+{#snippet FormatPage(name: string)}
+	<a
+		href="/{parentRoute}/{name}"
+		class="hover:text-ctp-lavender-50 flex h-full items-center px-2 transition-colors first:pl-0"
+		><div class="relative">
+			<span>{name}</span>
+			{#if route.includes(name)}
+				<hr
+					class="text-ctp-lavender border-1 absolute left-0 right-0 m-auto flex w-11/12 rounded-full"
+				/>
+			{/if}
+		</div></a
+	>
+{/snippet}
--- web/src/routes/formats/+layout.svelte
+++ web/src/routes/formats/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-	import FormatsNav from '$lib/components/FormatsNav.svelte';
+	import InnerNav from '$lib/components/InnerNav.svelte';
 	import { page } from '$app/state';
 
 	let { children } = $props();
 	let route = $derived(page.url.pathname.substring(1));
 </script>
 
-<FormatsNav {route} />
+<InnerNav {route} parentRoute={'formats'} pages={['monthly', 'motw', 'quest', 'bounty']} />
 
 {@render children?.()}
