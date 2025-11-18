08a688b3eb5daeff6ba0b0d427a1dbd7a58f35ea
bailey <spiritov_v@pm.me>
2025-11-18 16:53:06 -0700

    add placeholder pending snippet

--- web/src/routes/players/[id]/+page.svelte
+++ web/src/routes/players/[id]/+page.svelte
@@ -11,33 +11,30 @@
 	import type { PageData } from './$types';
 	import Header from './Header.svelte';
 
-	let showSoldier = $state(true);
-	let showDemo = $state(false);
-	let me = $state(true);
-
 	let { data: pageData }: { data: PageData } = $props();
 	let session: Session | null = $derived(pageData.session);
 
 	const playerID = Number.parseInt(page.params.id as string);
-	const { data } = await Client.GET('/internal/players/profile/{id}', {
+	const { data: playerData } = await Client.GET('/internal/players/profile/{id}', {
 		fetch: fetch,
 		params: {
 			path: { id: playerID }
 		}
 	});
 
-	let player: PlayerProfile | undefined = $derived(data);
-	$inspect(player);
+	let player: PlayerProfile | undefined = $derived(playerData);
 </script>
 
-<svelte:boundary>
+<svelte:boundary {pending}>
 	{#if player}
 		<Header {player} />
+	{:else}
+		todo no player
 	{/if}
 </svelte:boundary>
 
 <DataSection title="Bounties Claimed">
-	<div class="flex flex-wrap gap-x-4 gap-y-3">
+	<div class="grid w-full grid-cols-3 gap-2">
 		{#each { length: 1 }}
 			{@render testBounty()}
 		{/each}
@@ -45,7 +42,7 @@
 </DataSection>
 
 <DataSection title="Trophies">
-	<div class="flex flex-wrap gap-2">
+	<div class="grid w-full grid-cols-4 gap-2">
 		{#each { length: 1 }}
 			{@render testTrophy()}
 		{/each}
@@ -53,7 +50,7 @@
 </DataSection>
 
 <DataSection title="Quests Claimed">
-	<div class="flex flex-wrap gap-2">
+	<div class="grid w-full grid-cols-4 gap-2">
 		{#each { length: 1 }}
 			{@render testQuest()}
 		{/each}
@@ -86,11 +83,11 @@
 </DataSection>
 
 {#snippet testTrophy()}
-	<div class="bg-jfgray-900 relative flex h-16 w-48 shrink-0 overflow-hidden">
-		<span class="icon-[ant-design--trophy-outlined] text-ctp-teal absolute -left-8 z-0 size-16"
+	<div class="bg-jfgray-900 relative flex h-20 overflow-hidden">
+		<span class="icon-[ant-design--trophy-outlined] text-ctp-teal absolute -left-10 z-0 size-20"
 		></span>
-		<div class="absolute z-10 size-full">
-			<div class="ml-8 flex h-full flex-col justify-between p-1 pr-2">
+		<div class="absolute z-10 size-full p-1">
+			<div class="ml-10 flex h-full flex-col justify-between">
 				<div class="flex flex-col">
 					<div class="flex items-center gap-2">
 						<span class="text-ctp-yellow text-shadow-2xs text-shadow-ctp-yellow">1st</span>
@@ -102,39 +99,40 @@
 			</div>
 		</div>
 		<div
-			class="mask-l-from-50% mask-l-to-85% size-full bg-[url(https://tempusplaza.xyz/map-backgrounds/jump_escape_rc4.jpg)] bg-cover bg-right bg-no-repeat opacity-50"
+			style={`background-image: url("https://tempusplaza.xyz/map-backgrounds/jump_escape_rc4.jpg")`}
+			class="mask-l-from-50% mask-l-to-75% size-full bg-cover bg-right bg-no-repeat opacity-50"
 		></div>
 	</div>
 {/snippet}
 
 {#snippet testBounty()}
-	<div class="bg-jfgray-900 w-59 relative flex h-24 shrink-0 items-center overflow-hidden">
+	<div class="bg-jfgray-900 relative flex h-28 items-center overflow-hidden">
 		<span
-			class="icon-[ri--star-line] bg-linear-to-b from-ctp-lavender-950 to-ctp-lavender-50 absolute -left-10 z-0 size-24 bg-clip-content"
+			class="icon-[ri--star-line] bg-linear-to-b from-ctp-lavender-950 to-ctp-lavender-50 absolute -left-12 z-0 size-28"
 		></span>
-		<div class="absolute z-10 size-full">
-			<div class="ml-6 flex h-full flex-col p-1 pr-2">
+		<div class="absolute z-10 size-full p-2">
+			<div class="ml-6 flex h-full flex-col gap-0">
 				<div class="flex items-center gap-2">
 					<span class="text-shadow-xs text-shadow-ctp-lavender">100 keys</span>
 					<span class="text-2xl/4 opacity-90">for sub 4:00</span>
 				</div>
-
 				<span class="span-ellipsis ml-9 text-2xl/4">jump_escape_rc4</span>
 				<span class="text-ctp-lavender ml-9 mt-3 text-4xl/6 font-semibold">3:53.00</span>
 			</div>
 		</div>
 		<div
-			class="mask-l-from-50% mask-l-to-75% size-full bg-[url(https://tempusplaza.xyz/map-backgrounds/jump_escape_rc4.jpg)] bg-cover bg-right bg-no-repeat opacity-50"
+			style={`background-image: url("https://tempusplaza.xyz/map-backgrounds/jump_escape_rc4.jpg")`}
+			class="mask-l-from-50% mask-l-to-75% size-full bg-cover bg-right bg-no-repeat opacity-50"
 		></div>
 	</div>
 {/snippet}
 
 {#snippet testQuest()}
-	<div class="bg-jfgray-900 relative flex h-12 w-48 shrink-0 overflow-hidden">
-		<span class="icon-[ri--treasure-map-line] text-ctp-teal absolute -left-4 z-0 size-12"></span>
-		<div class="absolute z-10 size-full">
-			<div class="flex h-full flex-col justify-between p-1 pr-2">
-				<div class="ml-8 flex flex-col text-2xl/4">
+	<div class="bg-jfgray-900 relative flex h-14 overflow-hidden">
+		<span class="icon-[ri--treasure-map-line] text-ctp-teal absolute -left-6 z-0 size-14"></span>
+		<div class="absolute z-10 size-full p-1">
+			<div class="flex h-full flex-col">
+				<div class="ml-10 flex flex-col text-2xl/4">
 					<span class="span-ellipsis">jump_escape_rc4</span>
 					<span>completion</span>
 				</div>
@@ -144,4 +142,8 @@
 			class="mask-l-from-50% mask-l-to-75% size-full bg-[url(https://tempusplaza.xyz/map-backgrounds/jump_escape_rc4.jpg)] bg-cover bg-right bg-no-repeat opacity-50"
 		></div>
 	</div>
+{/snippet}
+
+{#snippet pending()}
+	todo loading
 {/snippet}
