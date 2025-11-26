aef7278860f80a83466de43c72fcb3159a9eaf1d
bailey <spiritov_v@pm.me>
2025-11-25 22:53:44 -0700

    allow initial message next to Input component

--- web/src/lib/components/input/Input.svelte
+++ web/src/lib/components/input/Input.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
   type Props = {
     label: string;
-    submitInput: (val: string) => InputError;
+    initialMessage?: string;
+    submitInput: (val: string) => Promise<InputError>;
   };
 
-  let { label, submitInput }: Props = $props();
+  let { label, initialMessage = '', submitInput }: Props = $props();
   let value: string = $state('');
 
-  let response: InputError = $state({
+  let response: InputError = $derived({
     error: false,
-    message: ''
+    message: initialMessage
   });
 </script>
 
 <!-- todo: response validation / rejection feedback -->
-<div class="flex items-center gap-2">
+<div class="flex h-12 items-center gap-2">
   <label
     for={label}
     class="relative mt-2 w-80 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50"
@@ -23,20 +24,20 @@
     <div class="flex h-10">
       <input
         bind:value
-        onkeypress={(event: KeyboardEvent) => {
+        onkeypress={async (event: KeyboardEvent) => {
           if (event.key === 'Enter') {
-            const error = submitInput(value);
+            const error = await submitInput(value);
             if (error) response = error;
           }
         }}
-        class="group peer w-full bg-jfgray-800 bg-clip-padding p-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
+        class="group peer size-full bg-jfgray-800 bg-clip-padding px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
         id={label}
         type="text"
       />
       <!-- svelte-ignore a11y_consider_explicit_label -->
       <button
-        onclick={() => {
-          response = submitInput(value);
+        onclick={async () => {
+          response = await submitInput(value);
         }}
         class="flex h-full w-12 cursor-pointer items-center justify-center bg-jfgray-800 transition-colors peer-focus:bg-jfgray-800"
       >
@@ -48,8 +49,10 @@
       </button>
     </div>
   </label>
-  <span class="{response.error === true ? 'text-ctp-red' : 'text-ctp-lavender'} transition-colors"
-    >{response.message}</span
+  <span
+    class="text-base {response.error === true
+      ? 'text-ctp-red'
+      : 'text-ctp-lavender'} transition-colors">{response.message}</span
   >
 </div>
 
