0c0818d7f38d50cd30d805f152a5282fd35ed29e
bailey <spiritov_v@pm.me>
2025-11-25 22:59:08 -0700

    fix bad import

--- web/src/routes/players/[id]/+page.svelte
+++ web/src/routes/players/[id]/+page.svelte
@@ -7,7 +7,7 @@
   import TableMap from '$lib/components/TableMap.svelte';
   import type { PlayerProfile, Session } from '$lib/schema';
   import type { PageData } from './$types';
-  import Header from '../../../lib/components/PlayerHeader.svelte';
+  import Header from '$lib/components/PlayerHeader.svelte';
 
   let { data }: { data: PageData } = $props();
   let session: Session | null = $derived(data.session);
