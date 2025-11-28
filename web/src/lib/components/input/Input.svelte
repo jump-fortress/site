cd500aa60ef94fe07b4631d2864642c3571b1ac7
bailey <spiritov_v@pm.me>
2025-11-27 21:33:58 -0700

    add pending state to Input component

--- web/src/lib/components/input/Input.svelte
+++ web/src/lib/components/input/Input.svelte
@@ -1,65 +1,72 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition';
+
   type Props = {
     label: string;
+    placeholder?: string;
     initialMessage?: string;
     submitInput: (val: string) => Promise<InputError>;
   };
 
-  let { label, initialMessage = '', submitInput }: Props = $props();
+  let { label, placeholder = '', initialMessage = '', submitInput }: Props = $props();
   let value: string = $state('');
 
-  let response: InputError = $derived({
-    error: false,
-    message: initialMessage
-  });
+  // response is a promise so we can use #await syntax to handle pending state
+  let response: Promise<InputError> = $derived(
+    Promise.resolve({
+      error: false,
+      message: initialMessage
+    })
+  );
 </script>
 
 <!-- todo: response validation / rejection feedback -->
 <div class="flex h-12 items-center gap-2">
   <label
     for={label}
-    class="relative mt-2 w-80 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50"
+    class="border-jfgray-700 focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50 relative mt-2 w-80 text-nowrap border-2 transition-colors"
   >
-    <span class="absolute -top-0.5 left-2 bg-jfgray-800 px-1 text-base leading-1">{label} </span>
+    <span class="bg-jfgray-800 leading-1 absolute -top-0.5 left-2 px-1 text-base">{label} </span>
     <div class="flex h-10">
       <input
         bind:value
+        {placeholder}
         onkeypress={async (event: KeyboardEvent) => {
           if (event.key === 'Enter') {
-            const error = await submitInput(value);
+            const error = submitInput(value);
             if (error) response = error;
           }
         }}
-        class="group peer size-full bg-jfgray-800 bg-clip-padding px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
+        class="bg-jfgray-800 text-ctp-lavender focus:bg-jfgray-900 group peer size-full bg-clip-padding px-1 transition-colors"
         id={label}
         type="text"
       />
       <!-- svelte-ignore a11y_consider_explicit_label -->
       <button
         onclick={async () => {
-          response = await submitInput(value);
+          response = submitInput(value);
         }}
-        class="flex h-full w-12 cursor-pointer items-center justify-center bg-jfgray-800 transition-colors peer-focus:bg-jfgray-800"
+        class="bg-jfgray-800 peer-focus:bg-jfgray-800 flex h-full w-12 cursor-pointer items-center justify-center"
       >
-        <span
-          class="icon-[ri--send-plane-line] {response.error === true
-            ? 'text-ctp-red'
-            : 'text-ctp-lavender'} transition-colors"
-        ></span>
+        {#await response}
+          <span in:fade class="icon-[ri--loader-3-line] text-ctp-lavender animate-spin"></span>
+        {:then response}
+          <span
+            in:fade
+            class="icon-[ri--send-plane-line] {response.error === true
+              ? 'text-ctp-red'
+              : 'text-ctp-lavender'}"
+          ></span>
+        {/await}
       </button>
     </div>
   </label>
-  <span
-    class="text-base {response.error === true
-      ? 'text-ctp-red'
-      : 'text-ctp-lavender'} transition-colors">{response.message}</span
-  >
+  {#await response then response}
+    <span
+      in:fade
+      class="text-base {response.error === true
+        ? 'text-ctp-red'
+        : 'text-ctp-lavender'} transition-colors">{response.message}</span
+    >
+  {/await}
 </div>
-
-<!-- <style lang="postcss">
-  @reference "../../app.css";
-
-  input {
-    @apply group peer w-full bg-jfgray-800 bg-clip-padding p-1 text-ctp-lavender transition-colors focus:bg-jfgray-900;
-  }
-</style> -->
--- web/src/routes/settings/+page.svelte
+++ web/src/routes/settings/+page.svelte
@@ -24,98 +24,104 @@
   let favoriteLauncher = $derived(player?.preferred_launcher ?? '');
 </script>
 
-<DataSection title="Profile">
-  <SelectButtons
-    title="fav class"
-    options={[
-      { src: rocket, value: 'Soldier' },
-      { src: sticky, value: 'Demo' }
-    ]}
-    selectedOption={favoriteClass}
-    onSelect={async (value: string) => {
-      // todo: feedback or message for successful update
-      const updated = updatePreferredClass(value);
-      favoriteClass = value;
-    }}
-  />
+{#if player}
+  <DataSection title="Profile">
+    <SelectButtons
+      title="fav class"
+      options={[
+        { src: rocket, value: 'Soldier' },
+        { src: sticky, value: 'Demo' }
+      ]}
+      selectedOption={favoriteClass}
+      onSelect={async (value: string) => {
+        // todo: feedback or message for successful update
+        const updated = updatePreferredClass(value);
+        favoriteClass = value;
+      }}
+    />
 
-  <SelectButtons
-    title="fav launcher"
-    options={[
-      { src: stock, value: 'Stock' },
-      { src: original, value: 'Original' },
-      { src: mangler, value: 'Mangler' },
-      { src: no_option, value: 'None' }
-    ]}
-    selectedOption={favoriteLauncher}
-    onSelect={async (value: string) => {
-      const updated = updatePreferredLauncher(value);
-      favoriteLauncher = value;
-    }}
-  />
+    <SelectButtons
+      title="fav launcher"
+      options={[
+        { src: stock, value: 'Stock' },
+        { src: original, value: 'Original' },
+        { src: mangler, value: 'Mangler' },
+        { src: no_option, value: 'None' }
+      ]}
+      selectedOption={favoriteLauncher}
+      onSelect={async (value: string) => {
+        const updated = updatePreferredLauncher(value);
+        favoriteLauncher = value;
+      }}
+    />
 
-  <div class="flex flex-col gap-2">
+    <div class="flex flex-col gap-2">
+      <Input
+        label={'request display name change'}
+        placeholder={player.display_name}
+        submitInput={async (val: string) => {
+          if (val === '') {
+            // todo: update with api request
+            return {
+              error: true,
+              message: 'empty input'
+            };
+          } else {
+            return {
+              error: false,
+              message: 'request sent! (is what this would say if it were implemented..)'
+            };
+          }
+        }}
+      />
+
+      <button class="settings-button">update avatar from steam</button>
+    </div>
+  </DataSection>
+
+  <DataSection title={'Rank'}>
+    {#if !player.soldier_division}
+      <button class="settings-button">request soldier placement</button>
+    {/if}
+    {#if !player.demo_division}
+      <button class="settings-button">request demo placement</button>
+    {/if}
+  </DataSection>
+
+  <DataSection title="Connections">
     <Input
-      label={'request display name change'}
+      label="Tempus ID"
+      placeholder={player.tempus_id ? player.tempus_id.toString() : ''}
       submitInput={async (val: string) => {
         if (val === '') {
-          // todo: update with api request
           return {
             error: true,
             message: 'empty input'
           };
         } else {
-          return {
-            error: false,
-            message: 'request sent! (is what this would say if it were implemented..)'
-          };
+          return await updateTempusID(parseInt(val));
         }
       }}
     />
 
-    <button class="settings-button">update avatar from steam</button>
-  </div>
-</DataSection>
+    <Input
+      label="Steam Trade URL"
+      placeholder={player.steam_trade_token}
+      submitInput={async (val: string) => {
+        if (val === '') {
+          return {
+            error: true,
+            message: 'empty input'
+          };
+        } else {
+          return await updateSteamTradeToken(val);
+        }
+      }}
+    />
 
-<DataSection title={'Rank'}>
-  {#if !player?.soldier_division}
-    <button class="settings-button">request soldier placement</button>
-  {/if}
-  {#if !player?.demo_division}
-    <button class="settings-button">request demo placement</button>
-  {/if}
-</DataSection>
-
-<DataSection title="Connections">
-  <Input
-    label="Tempus ID"
-    initialMessage={player?.tempus_id ? `set to ${player.tempus_id}` : ''}
-    submitInput={async (val: string) => {
-      if (val === '') {
-        return {
-          error: true,
-          message: 'empty input'
-        };
-      } else {
-        return await updateTempusID(parseInt(val));
-      }
-    }}
-  />
-
-  <Input
-    label="Steam Trade URL"
-    initialMessage={player?.steam_trade_token ? `set to ${player.steam_trade_token}` : ''}
-    submitInput={async (val: string) => {
-      if (val === '') {
-        return {
-          error: true,
-          message: 'empty input'
-        };
-      } else {
-        return await updateSteamTradeToken(val);
-      }
-    }}
-  />
-
-  <span>connect discord (not implemented)</span>
-</DataSection>
+    <span>connect discord (not implemented)</span>
+  </DataSection>
+{:else}
+  <!-- todo: error for missing player (redirect?) -->
+  <span></span>
+{/if}
