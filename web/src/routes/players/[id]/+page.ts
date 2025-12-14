ca391e8e31edc155fe1342dc4ff9f924540ed8ab
bailey <spiritov_v@pm.me>
2025-12-13 23:38:42 -0700

    return as playerProfile

--- web/src/routes/players/[id]/+page.ts
+++ web/src/routes/players/[id]/+page.ts
@@ -1,12 +1,7 @@
 import { Client } from '$lib/internalApi';
-import type { PlayerProfile } from '$lib/schema';
 import type { PageLoad } from './$types';
 
-// parent is used here since +page.ts cannot access locals
-export const load: PageLoad = async ({ fetch, params, parent }) => {
-  // todo: error handling
-  const session = (await parent()).session;
-
+export const load: PageLoad = async ({ fetch, params }) => {
   const { data } = await Client.GET('/internal/players/profile/{id}', {
     fetch: fetch,
     params: {
@@ -14,13 +9,7 @@ export const load: PageLoad = async ({ fetch, params, parent }) => {
     }
   });
 
-  if (!data) {
-    return {
-      player: null
-    };
-  }
   return {
-    player: data as PlayerProfile,
-    ownProfile: session ? data.id === session.id : false
+    playerProfile: data ?? null
   };
 };
