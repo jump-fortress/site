2943d2c8d19fb0a2d86aa3a428a1b8a90473f6ff
bailey <spiritov_v@pm.me>
2025-12-13 23:37:15 -0700

    add missing endpoints

--- web/src/lib/internalApi.ts
+++ web/src/lib/internalApi.ts
@@ -1,7 +1,7 @@
 // todo: review is this is acceptable:
 // use endpoint functions here only on non-page-load
 import createClient from 'openapi-fetch';
-import type { operations, paths, PlayerProfile } from './schema';
+import type { operations, paths, PlayerProfile, SelfPlayerRequest } from './schema';
 
 export const Client = createClient<paths>({
   baseUrl: 'http://localhost:5173/'
@@ -54,7 +54,7 @@ export async function updateTempusID(tempusId: number): Promise<InputError> {
 
   return {
     error: false,
-    message: 'success!'
+    message: 'updated'
   };
 }
 
@@ -77,7 +77,49 @@ export async function updateSteamTradeToken(url: string): Promise<InputError> {
 
   return {
     error: false,
-    message: 'success!'
+    message: 'updated'
+  };
+}
+
+export async function updateSteamAvatar(): Promise<InputError> {
+  const { error } = await Client.PUT('/internal/players/steamavatarurl', {
+    fetch: fetch
+  });
+
+  if (error) {
+    return {
+      error: true,
+      message: error.detail ?? 'Unknown error'
+    };
+  }
+
+  return {
+    error: false,
+    message: 'updated'
+  };
+}
+
+export async function updatePlayerDisplayName(id: string, name: string): Promise<InputError> {
+  const { error } = await Client.PUT('/internal/moderator/players/displayname/{id}/{name}', {
+    fetch: fetch,
+    params: {
+      path: {
+        id: id,
+        name: name
+      }
+    }
+  });
+
+  if (error) {
+    return {
+      error: true,
+      message: error.detail ?? 'Unknown error'
+    };
+  }
+
+  return {
+    error: false,
+    message: 'updated'
   };
 }
 
@@ -96,4 +138,91 @@ export async function getPlayerProfile(id: string): Promise<PlayerProfile | null
   }
 
   return null;
+}
+
+export async function updatePlayerSoldierDivision(
+  id: string,
+  division: string
+): Promise<InputError> {
+  const { error } = await Client.PUT(
+    '/internal/moderator/players/soldierdivision/{id}/{division}',
+    {
+      fetch: fetch,
+      params: {
+        path: {
+          id: id,
+          division: division
+        }
+      }
+    }
+  );
+
+  if (error) {
+    return {
+      error: true,
+      message: error.detail ?? 'Unknown error'
+    };
+  }
+
+  return {
+    error: false,
+    message: 'updated'
+  };
+}
+
+export async function updatePlayerDemoDivision(id: string, division: string): Promise<InputError> {
+  const { error } = await Client.PUT('/internal/moderator/players/demodivision/{id}/{division}', {
+    fetch: fetch,
+    params: {
+      path: {
+        id: id,
+        division: division
+      }
+    }
+  });
+
+  if (error) {
+    return {
+      error: true,
+      message: error.detail ?? 'Unknown error'
+    };
+  }
+
+  return {
+    error: false,
+    message: 'updated'
+  };
+}
+
+export async function createPlayerRequest(type: string, request: string) {
+  const { error } = await Client.PUT('/internal/players/requests/{request_type}/{request_string}', {
+    fetch: fetch,
+    params: {
+      path: {
+        request_type:
+          type as operations['insert-player-request']['parameters']['path']['request_type'],
+        request_string: request
+      }
+    }
+  });
+
+  if (error) {
+    return {
+      error: true,
+      message: error.detail ?? 'Unknown error'
+    };
+  }
+
+  return {
+    error: false,
+    message: 'request submitted'
+  };
+}
+
+export async function getPlayerRequests() {
+  const { data } = await Client.GET('/internal/players/requests', {
+    fetch: fetch
+  });
+
+  return data as SelfPlayerRequest[];
 }
