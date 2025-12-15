8bc7a1c264e126e749b396a70aad68b40684428b
bailey <spiritov_v@pm.me>
2025-12-14 20:25:37 -0700

    no styling gap if message is empty

--- web/src/lib/components/input/Response.svelte
+++ web/src/lib/components/input/Response.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import { fade } from 'svelte/transition';
 
-  let { response }: { response: InputError } = $props();
+  let { response = $bindable() }: { response: InputError } = $props();
+  let fadeMessage = $state(false);
+
+  // fade out success messages
+  if (!response.error) {
+    setTimeout(() => {
+      fadeMessage = true;
+    }, 5_000);
+  }
 </script>
 
-<span
-  in:fade
-  class="text-base {response.error === true
-    ? 'text-ctp-red'
-    : 'text-ctp-lavender'} transition-colors">{response.message}</span
->
+{#if response.message}
+  <span
+    in:fade
+    class="text-base transition-opacity
+  {response.error === true ? 'text-ctp-red' : 'text-ctp-lavender'} 
+    {fadeMessage ? 'opacity-0' : ''}">{response.message}</span
+  >
+{/if}
