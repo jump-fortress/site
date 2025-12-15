626a0745e14409b37f0528e5d33bb753ab300e45
bailey <spiritov_v@pm.me>
2025-12-14 20:24:36 -0700

    render children instead of label prop

--- web/src/lib/components/input/Button.svelte
+++ web/src/lib/components/input/Button.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import Response from '$lib/components/input/Response.svelte';
+  import type { Snippet } from 'svelte';
 
   type Props = {
-    label: string;
+    children: Snippet;
     responseMessage?: string;
     onSelect: () => Promise<InputError>;
   };
 
-  let { label, responseMessage = '', onSelect }: Props = $props();
+  let { children, responseMessage = '', onSelect }: Props = $props();
 
   // response is a promise so #await can be used to handle pending state
   let response: Promise<InputError> = $derived(
@@ -20,13 +21,15 @@
 
 <div class="flex items-center gap-2">
   <button
-    class="size-fit cursor-pointer rounded-lg border-2 border-jfgray-700 border-b-ctp-lavender/50 bg-jfgray-800 px-2 py-1 text-base transition-colors hover:bg-ctp-lavender-950/50"
+    class="border-jfgray-700 border-b-ctp-lavender/50 bg-jfgray-800 hover:bg-ctp-lavender-950/50 size-fit cursor-pointer rounded-lg border-2 px-2 py-1 text-base transition-colors"
     onclick={async () => {
       response = onSelect();
-    }}>{label}</button
+    }}>{@render children()}</button
   >
 
-  {#await response then response}
+  {#await response}
+    <Response response={{ error: false, message: '...' }} />
+  {:then response}
     <Response {response} />
   {/await}
 </div>
