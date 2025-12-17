85ff7b5e82a58c582133198a180be00497b82692
bailey <spiritov_v@pm.me>
2025-12-16 17:13:20 -0700

    add condensed button component

---
+++ web/src/lib/components/input/CheckButton.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import Response from '$lib/components/input/Response.svelte';
+  import type { Snippet } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  type Props = {
+    onSelect: () => Promise<InputError>;
+  };
+
+  let { onSelect }: Props = $props();
+
+  // response is a promise so #await can be used to handle pending state
+  let response: Promise<InputError> = $state(
+    Promise.resolve({
+      error: false,
+      message: ''
+    })
+  );
+</script>
+
+<div class="flex items-center gap-2">
+  <button
+    class="size-fit cursor-pointer border-2 border-jfgray-700 border-b-ctp-lavender/50 bg-jfgray-800 px-2 py-1 text-base transition-colors hover:bg-ctp-lavender-950/50"
+    onclick={async () => {
+      response = onSelect();
+    }}
+  >
+    {#await response}
+      <span in:fade class="icon-[ri--loader-3-line] animate-spin text-ctp-lavender"></span>
+    {:then response}
+      <span
+        in:fade
+        class={response.error === true
+          ? 'icon-[ri--close-line] text-ctp-red'
+          : 'icon-[ri--check-line] text-ctp-lavender'}
+      ></span>
+    {/await}
+  </button>
+</div>
