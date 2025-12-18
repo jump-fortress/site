130da3f47176029c7959ad80b4061ec9b1002a62
bailey <spiritov_v@pm.me>
2025-12-18 03:53:26 -0700

    add case-insensitivity to component

--- web/src/lib/components/DivisionTag.svelte
+++ web/src/lib/components/DivisionTag.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+  import { divisions } from '$lib/divisions';
+
   interface Props {
     div: string;
     styles?: string;
   }
 
   let { div, styles = '' }: Props = $props();
+  let caseInsensitiveDiv = $derived(div.toLowerCase());
 </script>
 
 <span
-  class="rounded-full px-2 pb-0.5 border-division-{div} border-x-3 text-division-{div} bg-jfgray-900/75 {styles} text-base"
+  class="rounded-full px-2 pb-0.5 border-division-{caseInsensitiveDiv} border-x-3 text-division-{caseInsensitiveDiv} bg-jfgray-900/75 {styles} w-fit text-base"
   >{div}</span
 >
