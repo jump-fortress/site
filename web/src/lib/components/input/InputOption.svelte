24a6438b5c300b433b51238a9322b1b49f060790
bailey <spiritov_v@pm.me>
2025-12-18 03:53:55 -0700

    rename component

---
+++ web/src/lib/components/input/InputOption.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  // index & selectedIndex are used for the InputAutofill component
+  type Props = {
+    value: string;
+    onSelect: (value: string) => void;
+    index?: number | null;
+    selectedIndex?: number | null;
+  };
+
+  let { value, onSelect, index = null, selectedIndex = null }: Props = $props();
+
+  let option: HTMLButtonElement | undefined = $state();
+
+  $effect(() => {
+    if (index === selectedIndex) {
+      option?.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+</script>
+
+<button
+  bind:this={option}
+  onmousedown={() => {
+    onSelect(value);
+  }}
+  class="group flex w-full text-left"
+  ><span
+    class="w-full rounded-lg px-2 group-hover:bg-jfgray-800
+    {index === selectedIndex ? 'bg-jfgray-800' : ''}">{value}</span
+  ></button
+>
