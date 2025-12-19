90ca8443b30241ae1c49cab02fbc9e7d3e146f3d
bailey <spiritov_v@pm.me>
2025-12-18 19:33:09 -0700

    create competition experimenting

--- web/src/routes/(moderation)/manage/(admin)/competitions/monthly/+page.svelte
+++ web/src/routes/(moderation)/manage/(admin)/competitions/monthly/+page.svelte
@@ -6,105 +6,125 @@
   import InputAutofill from '$lib/components/input/InputAutofill.svelte';
   import { onMount } from 'svelte';
   import DivisionTag from '$lib/components/DivisionTag.svelte';
+  import CompetitionManagePreview from '$lib/components/CompetitionManagePreview.svelte';
+  import type { Map } from '$lib/schema.js';
+  import InputButtons from '$lib/components/input/InputButtons.svelte';
 
   let validTime = $state(true);
 
   let { data } = $props();
 
-  let mapNames: string[] = $state([]);
+  let maps: Map[] = $state([]);
+  let mapNames: string[] = $derived(maps.map((m) => m.name));
 
-  let competitionMaps: string[] = $state([]);
+  let competitionMaps: MapWithDivisions[] = $state([]);
+  $inspect(competitionMaps);
+  let competitionMapNames: string[] = $derived(competitionMaps.map((m) => m.name));
 
-  function addCompetitionMap(map: string) {
-    if (!mapNames.includes(map)) {
+  function addCompetitionMap(map: Map | undefined) {
+    if (!map || !mapNames.includes(map.name)) {
       return { error: true, message: 'invalid map name' };
     }
-    if (competitionMaps.includes(map)) {
+    if (competitionMapNames.includes(map.name)) {
       return { error: true, message: 'map already added' };
     }
-    competitionMaps.push(map);
+    competitionMaps.push({ name: map.name, divisions: [] });
     return { error: false, message: '' };
   }
 
   onMount(async () => {
-    const maps = await data.maps;
-    if (maps) {
-      mapNames = maps.map((m) => m.name);
+    const dbMaps = await data.maps;
+    if (dbMaps) {
+      maps = dbMaps;
     }
   });
+
+  // todo: cleanup / componentize
 </script>
 
-<DataSection title={'Create'}>
-  <!-- date & time -->
-  <div class="flex h-12 items-center gap-2">
-    <label
-      for="date"
-      class="relative mt-2 w-50 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50"
-    >
-      <span class="absolute -top-1 left-2 bg-jfgray-800 px-1 text-base leading-1">date</span>
-      <div class="flex h-10">
-        <input
-          class="size-full px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
-          type="date"
-          id="date"
-          value={Temporal.Now.plainDateISO().toString()}
-        />
+{#await data.maps then maps}
+  {#if maps}
+    <DataSection title={'Create'}>
+      <!-- date & time -->
+      <div class="flex h-12 items-center gap-2">
+        <label
+          for="date"
+          class="relative mt-2 w-50 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50"
+        >
+          <span class="absolute -top-1 left-2 bg-jfgray-800 px-1 text-base leading-1">date</span>
+          <div class="flex h-10">
+            <input
+              class="size-full px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
+              type="date"
+              id="date"
+              value={Temporal.Now.plainDateISO().toString()}
+            />
+          </div>
+        </label>
+        <label
+          for="time"
+          class="relative mt-2 w-50 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50 has-invalid:border-ctp-red"
+        >
+          <span class="absolute -top-1 left-2 bg-jfgray-800 px-1 text-base leading-1">time</span>
+          <div class="flex h-10">
+            <input
+              oninput={(e) => {
+                const valid = e.currentTarget.checkValidity();
+                validTime = valid;
+              }}
+              class="size-full px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
+              type="time"
+              id="time"
+              step="1800"
+              value={Temporal.Now.plainTimeISO().round('hour').toString({ smallestUnit: 'minute' })}
+            />
+          </div>
+        </label>
+        {#if !validTime}
+          <span>please set a time in steps of 30 minutes (:00 or :30)</span>
+        {/if}
+        <div class="ml-auto">
+          <span class="text-ctp-lavender">timezone: </span>
+          <span>{Temporal.Now.timeZoneId()}</span>
+        </div>
       </div>
-    </label>
-    <label
-      for="time"
-      class="relative mt-2 w-50 border-2 border-jfgray-700 text-nowrap transition-colors focus-within:border-ctp-lavender-50/50 hover:border-ctp-lavender-50/50 has-invalid:border-ctp-red"
-    >
-      <span class="absolute -top-1 left-2 bg-jfgray-800 px-1 text-base leading-1">time</span>
-      <div class="flex h-10">
-        <input
-          oninput={(e) => {
-            const valid = e.currentTarget.checkValidity();
-            validTime = valid;
-          }}
-          class="size-full px-1 text-ctp-lavender transition-colors focus:bg-jfgray-900"
-          type="time"
-          id="time"
-          step="1800"
-          value={Temporal.Now.plainTimeISO().round('hour').toString({ smallestUnit: 'minute' })}
-        />
-      </div>
-    </label>
-    {#if !validTime}
-      <span>please set a time in steps of 30 minutes (:00 or :30)</span>
-    {/if}
-    <div class="ml-auto">
-      <span class="text-ctp-lavender">timezone: </span>
-      <span>{Temporal.Now.timeZoneId()}</span>
-    </div>
-  </div>
-  <!-- add divisions & maps -->
-  <InputSelect
-    label={'class'}
-    options={['Soldier', 'Demo']}
-    submitOption={async (val: string) => {
-      return { error: false, message: '' };
-    }}
-  />
-  {#await data.maps then maps}
-    {#if maps}
+      <!-- add divisions & maps -->
+      <InputSelect
+        label={'class'}
+        options={['Soldier', 'Demo']}
+        submitOption={async (val: string) => {
+          return { error: false, message: '' };
+        }}
+      />
+
       <InputAutofill
         label={'add map'}
         options={maps.map((m) => m.name)}
         submitOption={async (val: string) => {
-          return addCompetitionMap(val);
+          return addCompetitionMap(maps.find((m) => m.name == val));
         }}
       />
-    {/if}
-  {/await}
 
-  <!-- select divisions for maps -->
-  {#each competitionMaps as cm}
-    <div class="flex items-center gap-2">
-      <span>{cm}</span>
-      {#each divisions.slice(0, -1) as div}
-        <DivisionTag {div} />
+      <!-- select divisions for maps -->
+      {#each competitionMaps as map}
+        <div class="flex items-center gap-2">
+          <span>{map.name}</span>
+          <InputSelect
+            label={'select divisions'}
+            options={divisions.concat('None')}
+            submitOption={async (val: string) => {
+              if (val === 'None') {
+                map.divisions = [];
+              } else {
+                map.divisions.push(val);
+              }
+              return { error: false, message: '' };
+            }}
+          />
+        </div>
       {/each}
-    </div>
-  {/each}
-</DataSection>
+
+      <CompetitionManagePreview maps={competitionMaps} />
+    </DataSection>
+  {/if}
+{/await}
