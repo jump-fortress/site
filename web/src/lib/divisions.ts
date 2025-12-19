9583c4a5a30f67074d4657cb474fdefdfc17a24e
bailey <spiritov_v@pm.me>
2025-12-18 23:47:05 -0700

    add division comparator

--- web/src/lib/divisions.ts
+++ web/src/lib/divisions.ts
@@ -7,3 +7,11 @@ export const divisions: string[] = [
   'Steel',
   'Wood'
 ];
+
+// expects divisions to have first letter uppercase
+export function compareDivisions(a: string, b: string): number {
+  const ai = divisions.indexOf(a);
+  const bi = divisions.indexOf(b);
+  if (ai === bi) return 0;
+  return ai > bi ? 1 : -1;
+}
