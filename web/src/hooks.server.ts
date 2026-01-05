9cacb5a5303cf48a8afb6fea06756f28546cf6cf
bailey <spiritov_v@pm.me>
2026-01-05 00:45:00 -0700

    get Session async

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -6,21 +6,19 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
   //check for session before making a request
   if (!event.locals.session) {
-    const { data, error } = await Client.GET('/internal/session', {
+    const data = await Client.GET('/internal/session', {
       baseUrl: config.apiBaseUrl,
       headers: event.request.headers,
       credentials: 'include'
+    }).then((response) => {
+      return response.data;
     });
 
-    event.locals.session = data ?? null;
-
-    if (error && error.status !== 401) {
-      console.error('There was an error retrieving the session', error);
-    }
+    event.locals.session = data;
 
     // making internal api request
     if (event.url.pathname.startsWith('/internal')) {
-      const result = await fetch(new URL(event.url.pathname, config.apiBaseUrl), event.request);
+      const result = fetch(new URL(event.url.pathname, config.apiBaseUrl), event.request);
       return result;
     }
   }
--- web/src/routes/logout/+page.svelte
+++ web/src/routes/logout/+page.svelte
@@ -14,5 +14,5 @@
     }
   }
 
-  await handleSignOut();
+  handleSignOut();
 </script>
