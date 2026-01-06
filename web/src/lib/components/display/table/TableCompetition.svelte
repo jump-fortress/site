bf6d90d7e55225253a024973ca979028871e16c5
bailey <spiritov_v@pm.me>
2026-01-06 04:51:03 -0700

    TableCompetition component

---
+++ web/src/lib/components/display/table/TableCompetition.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import ClassImage from '../ClassImage.svelte';
+
+  import type { Competition } from '$lib/schema';
+
+  type Props = {
+    competition: Competition;
+    format: string;
+    formatId: number;
+  };
+
+  let { competition, format, formatId }: Props = $props();
+</script>
+
+<div class="group flex h-8 cursor-pointer items-center gap-2 rounded-box px-2 hover:bg-base-900">
+  <ClassImage selected={competition.class} />
+  <span class="truncate group-hover:underline">{format} #{formatId}</span>
+</div>
