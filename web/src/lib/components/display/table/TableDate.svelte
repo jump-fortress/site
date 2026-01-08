e4da8638dc7960d3d018510593599609ca09ebfe
bailey <spiritov_v@pm.me>
2026-01-07 21:14:45 -0700

    update styling

--- web/src/lib/components/display/table/TableDate.svelte
+++ web/src/lib/components/display/table/TableDate.svelte
@@ -14,7 +14,7 @@
 <div class="relative">
   <span class="peer">{formatRelative(ms)}</span>
   <div
-    class="invisible absolute -top-full left-1/2 -translate-x-1/2 rounded-box bg-base-900 px-2 py-1 text-nowrap peer-hover:visible hover:visible">
+    class="invisible absolute -top-1 left-1/2 -translate-x-1/2 rounded-box bg-base-900 px-2 py-1 text-nowrap peer-hover:visible hover:visible">
     <span>
       {formatDate(ms)}
       <span class="text-primary">{formatTime(ms)}</span>
--- web/src/lib/components/display/table/TableMap.svelte
+++ web/src/lib/components/display/table/TableMap.svelte
@@ -6,11 +6,10 @@
   let { map }: Props = $props();
 </script>
 
-<div
-  class="relative grid size-full place-content-center mask-x-from-90% object-cover object-center">
+<div class="relative grid size-full scale-x-105 place-content-center mask-x-from-97%">
   <span class="relative z-10 truncate text-shadow-base-900 text-shadow-sm">{map}</span>
   <img
-    class="absolute size-full"
+    class="absolute size-full object-cover"
     src="https://tempusplaza.xyz/map-backgrounds/{map}.jpg"
     alt=""
     loading="lazy" />
