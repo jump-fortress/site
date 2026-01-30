a29a57cd578719f73db884591d7b633bd178ee18
bailey <spiritov_v@pm.me>
2026-01-30 00:29:50 -0700

    add Table preset

---
+++ web/src/lib/components/display/table/presets/TableEvents.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Table from '$lib/components/display/table/Table.svelte';
+  import TableEvent from '$lib/components/display/table/TableEvent.svelte';
+  import TableMaps from '$lib/components/display/table/TableMaps.svelte';
+  import TemporalDate from '$lib/components/display/TemporalDate.svelte';
+  import type { EventWithLeaderboards } from '$lib/schema';
+
+  type Props = {
+    data: EventWithLeaderboards[];
+    onclick: (ewl: EventWithLeaderboards) => void;
+  };
+
+  let { data, onclick }: Props = $props();
+</script>
+
+<Table {data}>
+  {#snippet header()}
+    <th class="w-event"></th>
+    <th></th>
+    <th class="w-date"></th>
+  {/snippet}
+  {#snippet row({ event, leaderboards }: EventWithLeaderboards)}
+    <td
+      onclick={() => {
+        onclick({ event, leaderboards });
+      }}><TableEvent {event} /></td>
+    <td class="flex"><TableMaps leaderboards={leaderboards ?? []} /></td>
+    <td class="table-date"><TemporalDate datetime={event.starts_at} /></td>
+  {/snippet}
+</Table>
