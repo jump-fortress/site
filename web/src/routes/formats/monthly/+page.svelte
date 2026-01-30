22ce193b6c0fa03bb3f9b7026389283d55dc95c1
bailey <spiritov_v@pm.me>
2026-01-29 18:31:57 -0700

    skeleton monthly pages

---
+++ web/src/routes/formats/monthly/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Client } from '$lib/api/api';
+  import { ApiPaths } from '$lib/schema';
+</script>
+
+{#await Client.GET(ApiPaths.get_event_kinds, { params: { path: { event_kind: 'monthly' } } })}
+  <span></span>
+{:then { data: monthlies }}
+  <span>under construction</span>
+  {#each monthlies as monthly}
+    <span>{monthly.event.kind} {monthly.event.kind_id}</span>
+  {/each}
+{/await}
