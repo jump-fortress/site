b98497b5cfb990b39c6d7a61ee75600f7d1d0be1
bailey <spiritov_v@pm.me>
2026-01-29 18:31:38 -0700

    manage events page

---
+++ web/src/lib/helpers/temporal.ts
@@ -0,0 +1,53 @@
+import { Temporal } from 'temporal-polyfill';
+
+// YYYY-MM-DD T HH:MM:ss (optional ss)
+export function validDateTime(dt: string): string {
+  return /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2})?Z$/.test(dt) ? dt : '2000-01-01T12:00:00Z';
+}
+
+const units = new Map<'year' | 'month' | 'day' | 'hour' | 'minute', number>([
+  ['year', 24 * 60 * 60 * 1000 * 365],
+  ['month', (24 * 60 * 60 * 1000 * 365) / 12],
+  ['day', 24 * 60 * 60 * 1000],
+  ['hour', 60 * 60 * 1000],
+  ['minute', 60 * 1000]
+]);
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric'
+});
+
+const timeFormatter = new Intl.DateTimeFormat(undefined, {
+  hour12: false,
+  hour: '2-digit',
+  minute: '2-digit',
+  timeZoneName: 'shortOffset',
+  timeZone: Temporal.Now.timeZoneId()
+});
+
+const relativeFormatter = new Intl.RelativeTimeFormat(undefined, { numeric: 'always' });
+
+export function formatDate(time: number): string {
+  return dateFormatter.format(time).replace(',', '');
+}
+
+export function formatTime(time: number): string {
+  return timeFormatter.format(time).replace('GMT', 'UTC');
+}
+
+export function formatRelative(time: number): string {
+  const now = Temporal.Now.instant().epochMilliseconds;
+  const diff = time - now;
+
+  for (const [unit, ms] of units)
+    if (Math.abs(diff) > ms) {
+      return relativeFormatter.format(Math.round(diff / ms), unit);
+    }
+  return relativeFormatter.format(Math.round(diff / (60 * 1000)), 'minute');
+}
+
+export function datetimeToMs(date: string): number {
+  return Temporal.Instant.from(date).epochMilliseconds;
+}
---
+++ web/src/routes/(mod)/manage/formats/+page.svelte
@@ -0,0 +1,213 @@
+<script lang="ts">
+  import { Client } from '$lib/api/api';
+  import ClassSelect from '$lib/components/display/ClassSelect.svelte';
+  import EventHeader from '$lib/components/display/EventHeader.svelte';
+  import Table from '$lib/components/display/table/Table.svelte';
+  import TableEvent from '$lib/components/display/table/TableEvent.svelte';
+  import TableMaps from '$lib/components/display/table/TableMaps.svelte';
+  import TemporalDate from '$lib/components/display/TemporalDate.svelte';
+  import Button from '$lib/components/input/Button.svelte';
+  import Errors from '$lib/components/input/Errors.svelte';
+  import Input from '$lib/components/input/Input.svelte';
+  import Select from '$lib/components/input/Select.svelte';
+  import Section from '$lib/components/layout/Section.svelte';
+  import { datetimeToMs, validDateTime } from '$lib/helpers/temporal';
+  import { ApiPaths, type Event, type EventWithLeaderboards, type Leaderboard } from '$lib/schema';
+  import { Temporal } from 'temporal-polyfill';
+
+  let event_id: number = $state(0);
+  let player_class: 'Soldier' | 'Demo' = $state('Soldier');
+  let event_kind: string = $state('monthly');
+  let event_kind_id: number = $state(0);
+  let visible_date: string = $state('');
+  let visible_time: string = $state('');
+  let start_date: string = $state('');
+  let start_time: string = $state('');
+
+  const visible_at: string = $derived(validDateTime(`${visible_date}T${visible_time}:00Z`));
+  const starts_at: string = $derived(validDateTime(`${visible_date}T${visible_time}:00Z`));
+
+  let leaderboards: Leaderboard[] = $state([{ event_id: 0, id: 0, map: 'jump_beef' }]);
+
+  let reloadEvents: boolean = $state(true);
+  let mode: 'create' | 'update' = $state('create');
+
+  let oerror: OpenAPIError = $state(undefined);
+
+  // update event with the accessed event
+  function loadEvent({ event: e, leaderboards: l }: EventWithLeaderboards): void {
+    event_id = e.id;
+    player_class = e.player_class;
+    event_kind = e.kind;
+    event_kind_id = e.kind_id;
+    visible_date = e.visible_at.substring(0, e.visible_at.indexOf('T'));
+    visible_time = e.visible_at.substring(
+      e.visible_at.indexOf('T') + 1,
+      e.visible_at.indexOf('Z') - 3
+    );
+    console.log(visible_time);
+    start_date = e.starts_at.substring(0, e.starts_at.indexOf('T'));
+    start_time = e.starts_at.substring(
+      e.visible_at.indexOf('T') + 1,
+      e.visible_at.indexOf('Z') - 3
+    );
+    console.log(start_time);
+    event.ends_at = e.ends_at;
+    leaderboards = l ?? [];
+  }
+
+  const event = $derived.by(() => {
+    const event: Event = {
+      id: event_id,
+      kind: event_kind as Event['kind'],
+      kind_id: event_kind_id,
+      player_class: player_class,
+      visible_at: visible_at,
+      starts_at: starts_at,
+      ends_at: starts_at,
+      created_at: starts_at
+    };
+    return event;
+  });
+</script>
+
+<Section label={mode}>
+  <EventHeader event={{ event: event, leaderboards: leaderboards }} />
+  <div class="flex justify-end gap-1">
+    <span class="text-primary">visible at</span>
+    <TemporalDate datetime={visible_at} />
+    <span class="icon-[mdi--monitor]"></span>
+  </div>
+
+  <Errors {oerror} />
+
+  <div class="flex">
+    <ClassSelect bind:player_class />
+  </div>
+
+  <div class="flex flex-col gap-1">
+    <Select
+      label="kind"
+      type="button"
+      options={['monthly']}
+      bind:value={event_kind}
+      withSubmit={false}
+      onsubmit={async () => {
+        return true;
+      }} />
+
+    <div class="flex justify-between">
+      <div class="flex grow flex-col gap-1">
+        <div class="flex gap-1">
+          <Input
+            label="visible date"
+            type="date"
+            withSubmit={false}
+            bind:value={visible_date}
+            onsubmit={async () => {
+              return true;
+            }} />
+          <Input
+            label="visible time"
+            type="time"
+            withSubmit={false}
+            bind:value={visible_time}
+            onsubmit={async () => {
+              return true;
+            }} />
+        </div>
+
+        <div class="flex gap-1">
+          <Input
+            label="start date"
+            type="date"
+            withSubmit={false}
+            bind:value={start_date}
+            onsubmit={async () => {
+              return true;
+            }} />
+          <Input
+            label="visible time"
+            type="time"
+            withSubmit={false}
+            bind:value={start_time}
+            onsubmit={async () => {
+              return true;
+            }} />
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <span
+          >input timezone <span class="text-primary">UTC</span>
+          <span></span>
+        </span>
+        <span
+          >your timezone <span class="text-primary"
+            >{Temporal.Now.timeZoneId()} ({Temporal.Now.zonedDateTimeISO().offset} UTC)</span
+          ></span>
+      </div>
+    </div>
+
+    <div class="flex gap-1">
+      {#if mode === 'create'}
+        <Button
+          onsubmit={async () => {
+            const resp = await Client.POST(ApiPaths.create_event, {
+              body: event
+            });
+            oerror = resp.error;
+            reloadEvents = !reloadEvents;
+            return resp.response.ok;
+          }}>create</Button>
+      {:else}
+        <Button
+          onsubmit={async () => {
+            const resp = await Client.POST(ApiPaths.update_event, {
+              body: event
+            });
+            oerror = resp.error;
+            reloadEvents = !reloadEvents;
+            return resp.response.ok;
+          }}>update</Button>
+        <Button
+          onsubmit={async () => {
+            const resp = await Client.DELETE(ApiPaths.cancel_event, {
+              params: { path: { event_id: event.id } }
+            });
+            oerror = resp.error;
+            reloadEvents = !reloadEvents;
+            return resp.response.ok;
+          }}>delete</Button>
+      {/if}
+    </div>
+  </div>
+</Section>
+
+<!-- todo: get all events! -->
+<Section label={'updatable events'}>
+  {#key reloadEvents}
+    {#await Client.GET(ApiPaths.get_full_events)}
+      <span></span>
+    {:then { data: ewls }}
+      {@const now = Temporal.Now.instant().epochMilliseconds}
+      {@const editable = ewls?.filter(({ event }) => datetimeToMs(event.starts_at) > now) ?? []}
+      <Table data={editable}>
+        {#snippet header()}
+          <th class="w-event"></th>
+          <th></th>
+          <th class="w-date"></th>
+        {/snippet}
+        {#snippet row({ event, leaderboards }: EventWithLeaderboards)}
+          <td
+            onclick={() => {
+              mode = 'update';
+              loadEvent({ event, leaderboards });
+            }}><TableEvent {event} /></td>
+          <td><TableMaps leaderboards={leaderboards ?? []} /></td>
+          <td class="table-date"><TemporalDate datetime={event.starts_at} /></td>
+        {/snippet}
+      </Table>
+    {/await}
+  {/key}
+</Section>
