171e5f0b6ba1e4a15bf3dc2db3389d31056dd1d9
bailey <spiritov_v@pm.me>
2026-02-19 19:53:09 -0700

    motw timeslots migration

---
+++ api/db/migrations/20260220003147_motw.down.sql
@@ -0,0 +1,2 @@
+drop table player_motw_timeslot;
+drop table motw_timeslot;
---
+++ api/db/migrations/20260220003147_motw.up.sql
@@ -0,0 +1,12 @@
+create table motw_timeslot(
+  id integer not null primary key autoincrement,
+  starts_at datetime not null
+);
+
+create table player_motw_timeslot(
+  timeslot_id integer not null default 1,
+  player_id text not null,
+
+  foreign key (timeslot_id) references motw_timeslot (id),
+  foreign key (player_id) references player (id)
+);
