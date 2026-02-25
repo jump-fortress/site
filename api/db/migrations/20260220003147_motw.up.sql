8c7473b19c49cbbac30ee5de499592a53731294f
bailey <spiritov_v@pm.me>
2026-02-24 21:32:45 -0700

    set default timeslots

--- api/db/migrations/20260220003147_motw.up.sql
+++ api/db/migrations/20260220003147_motw.up.sql
@@ -3,6 +3,11 @@ create table motw_timeslot(
   starts_at datetime not null
 );
 
+insert into motw_timeslot (starts_at) values 
+  ("2026-01-01 00:00:00+00:00"),
+  ("2026-01-01 06:00:00+00:00"),
+  ("2026-01-01 18:00:00+00:00");
+
 create table player_motw_timeslot(
   timeslot_id integer not null default 1,
   player_id text not null unique,
